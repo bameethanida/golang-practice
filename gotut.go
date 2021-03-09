8d0a3e917624d6d41111afc83d0d56301399a988
Thanida Jongarnon <bameejrn@outlook.com>
2021-03-09 16:19:04 +0700

    basic page

---
+++ gotut.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"html/template"
+)
+
+type NewsAggPage struct {
+    Title string
+    News string
+}
+
+func newsAggHandler(w http.ResponseWriter, r *http.Request) {
+	p := NewsAggPage{Title: "Amazing News Aggregator", News: "some news"}
+    t, _ := template.ParseFiles("basictemplating.html")
+    fmt.Println(t.Execute(w, p))
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w,"<h1>Hello World</h1>")
+	
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/agg/", newsAggHandler)
+	http.ListenAndServe(":8000", nil)
+}
