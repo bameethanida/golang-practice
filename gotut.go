afd89dcf11472b28715a7ee7b564b6339e84743e
Thanida Jongarnon <bameejrn@outlook.com>
2021-03-14 22:46:33 +0700

    fixing error

--- gotut.go
+++ gotut.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"html/template"
 	"encoding/xml"
 	"io/ioutil"
 	"sync"
@@ -35,40 +34,38 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Whoa, Go is neat!</h1>")
 }
 
-func newsRoutine(c chan News, Location string) {
-	defer wg.Done()
-	var n News
-	resp, _ := http.Get(Location)
-	bytes, _ := ioutil.ReadAll(resp.Body)
-	xml.Unmarshal(bytes, &n)
-	resp.Body.Close()
-	c <- n
+func newsRoutine(c chan News, Location string){
+    defer wg.Done()
+    var n News
+    resp, _ := http.Get(Location)
+    bytes, _ := ioutil.ReadAll(resp.Body)
+    xml.Unmarshal(bytes, &n)
+    resp.Body.Close()
+    c <- n
 }
 
 func newsAggHandler(w http.ResponseWriter, r *http.Request) {
 	var s Sitemapindex
-	var n News
+	//var n News
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
     bytes, _ := ioutil.ReadAll(resp.Body)
-	xml.Unmarshal(resp, &s)
+	xml.Unmarshal(bytes, &s)
 	news_map := make(map[string]NewsMap)
 	resp.Body.Close()
 	queue := make(chan News, 30)
 	for _, Location := range s.Locations {
-		wg.Add(1)
-		go newRoutine(queue, Location)
-	}
-
-	wg.Wait()
-	close(queue)
+        wg.Add(1)
+        go newsRoutine(queue, Location)
+    }
+    wg.Wait()
+    close(queue)
 	for elem := range queue {
 		for idx, _ := range elem.Keywords {
 			news_map[elem.Titles[idx]] = NewsMap{elem.Keywords[idx], elem.Locations[idx]}
 		}
 	}
-
-	p := NewsAggPage{Title: "Amazing News Aggregator", News: news_map}
-    t, _ := template.ParseFiles("basictemplating.html")
+	//p := NewsAggPage{Title: "Amazing News Aggregator", News: news_map}
+    //t, _ := template.ParseFiles("basictemplating.html")
 }
 
 func main() {
